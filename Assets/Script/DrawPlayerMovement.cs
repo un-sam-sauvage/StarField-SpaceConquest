fbd669fb88bc3f91aba9443146a83a6712a2267c
sam <samuel.brb19@gmail.com>
2021-02-06 22:51:39 +0100

    début A*

---
+++ Assets/Script/DrawPlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DrawPlayerMovement : MonoBehaviour
+{
+    public static List<Vector3> GetMovableTile(int range, Vector3 start)
+    {
+        List<Vector3> tiles = new List<Vector3>();
+        tiles.Add(start);
+        for (int i = 0; i < range; i++)
+        {
+            foreach (var tile in tiles)
+            {
+                tiles = tiles.Union(GetAdjacentTiles(tile)).ToList();
+            }
+        }
+
+        tiles.Remove(start);
+        return tiles;
+    }
+
+    public static List<Vector3> GetAdjacentTiles(Vector3 start)
+    {
+        List<Vector3> tiles = new List<Vector3>();
+        tiles.Add(start + Vector3.left);
+        tiles.Add(start + Vector3.right);
+        tiles.Add(start + Vector3.up + .5f * Vector3.right);
+        tiles.Add(start + Vector3.up + .5f * Vector3.left);
+        tiles.Add(start + Vector3.down + .5f * Vector3.right);
+        tiles.Add(start + Vector3.down + .5f * Vector3.left);
+        return tiles;
+    }
+}
--- Assets/Script/MouseMovementPlayer.cs
+++ Assets/Script/MouseMovementPlayer.cs
@@ -1,21 +1,31 @@
 ﻿using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MouseMovementPlayer : MonoBehaviour
 {
-    public Grid grid;
+    public Tilemap tilemap;
+    public Tile tileToMove;
+    public int range;
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int coordinate = tilemap.WorldToCell(mouseWorldPos);
+        Vector3 positionPlayer = tilemap.GetCellCenterLocal(coordinate);
         //move the player to the cell which was clicked
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int coordinate = grid.WorldToCell(mouseWorldPos);
-            Vector3 positionPlayer = grid.GetCellCenterLocal(coordinate);
-            
             gameObject.transform.DOMove(positionPlayer,.7f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            foreach (var tile in DrawPlayerMovement.GetMovableTile(range,tilemap.WorldToCell(gameObject.transform.position)))
+            {
+                tilemap.SetTile(tilemap.WorldToCell(tile),tileToMove);
+            }
         }
     }
 }
