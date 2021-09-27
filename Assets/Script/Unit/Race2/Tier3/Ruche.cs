3b0ac94bc63e2ae283e4fd1b59acbdad7cf9e052
sam <samuel.brb19@gmail.com>
2021-09-27 14:43:57 +0200

    Tier 3 Race 2

---
+++ Assets/Script/Unit/Race2/Tier3/Ruche.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Ruche : MonoBehaviour
+{
+    public int nbTurnAlive;
+    private GameManager _gm;
+    public void Start()
+    {
+        _gm = GameManager.instance;
+        _gm.nextTurn.AddListener(ChangeTurn);
+    }
+
+    void ChangeTurn()
+    {
+        if (EnemiToDamage().Count > 0)
+        {
+            foreach (var unit in EnemiToDamage())
+            {
+                unit.life -= 2;
+                if (unit.life <=0)
+                {
+                    unit.unitAnimator.SetBool("IsDead", true);
+                }
+            }
+        }
+        nbTurnAlive++;
+        if (nbTurnAlive >= 3)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    List<Unit> EnemiToDamage()
+    {
+        List<Unit> enemis = new List<Unit>();
+        foreach (var player in _gm.players)
+        {
+            if (player != _gm.currentPlayer)
+            {
+                foreach (var unit in player.GetComponent<PlayerInfos>().units)
+                {
+                    if (unit.GetPos() == gameObject.transform.position)
+                    {
+                        enemis.Add(unit);
+                    }
+                }
+            }
+        }
+        return enemis;
+    }
+}
---
+++ Assets/Script/Unit/Race2/Tier3/SpecialVaisseauRuche.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SpecialVaisseauRuche : SpecialEffectShip
+{
+    private GameManager _gm;
+    
+    private Unit _unit;
+    
+    private List<Vector3> tilesAround;
+    
+    private bool _isUsingSpecialEffect;
+    
+    public GameObject ruche;
+    private GameObject _currentRuche;
+    public override void Use(Unit unit)
+    {
+        if (_currentRuche == null)
+        {
+            _isUsingSpecialEffect = true;
+            _gm = GameManager.instance;
+            _unit = unit;
+            tilesAround = StaticVoid.GetAdjacentTiles(_unit.GetPos());
+            tilesAround.Remove(_unit.GetPos());
+            _gm.ShowUIforUnit(false);
+            foreach (var tile in tilesAround)
+            {
+                _gm.moveTilemap.SetTile(_gm.moveTilemap.WorldToCell(tile),_gm.movementTile);
+            }
+        }
+        else
+        {
+            Debug.Log("il y a déjà une ruche");
+        }
+    }
+
+    void SetRuche(Vector3Int ruchePos)
+    {
+        _currentRuche = Instantiate(ruche, ruchePos, quaternion.identity);
+        _gm.ShowUIforUnit(true);
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && _isUsingSpecialEffect)
+        {
+            foreach (var tile in tilesAround)
+            {
+                if (Input.mousePosition == tile)
+                {
+                   SetRuche(_gm.moveTilemap.WorldToCell(tile));
+                   _isUsingSpecialEffect = false;
+                }
+            }
+
+        }
+    }
+}
